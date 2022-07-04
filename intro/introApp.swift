241062b1d1120e41daae4e0193404700a9c8f99a
mishaa <muru_mish@hotmail.com>
2022-07-03 20:11:29 -0400

    Initial Commit

---
+++ intro/introApp.swift
@@ -0,0 +1,17 @@
+//
+//  introApp.swift
+//  intro
+//
+//  Created by Mishaa on 2022-07-03.
+//
+
+import SwiftUI
+
+@main
+struct introApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
